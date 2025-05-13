f3256731200cc31cf95220c2ae45455d26af611f
nathanseixas <nathan19042004@gmail.com>
2025-05-13 10:03:53 -0300

    tela de login pronta + Front End Finaliozado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pagess/login/login.component';
+import { SignUpComponent } from './pagess/signup/signup.component';
 
 export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent
+    },
+
+    {
+        path: "signup",
+        component: SignUpComponent
     }
 ];
